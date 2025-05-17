ec10c5e7b882f27ed2bb240124023bc70b814006
ChitTun18 <chithtun1810@gmail.com>
2025-05-17 13:42:21 +0630

    controller

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,27 +1,17 @@
 const express = require('express');
-const Workout = require('../models/workoutmodel');
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout,
+} = require('../controllers/workoutController');
 
 const router = express.Router();
 // gET all workouts
-router.get('/', (req, res) => {
-    res.json({ mssg: ' gET Workouts route' });
-})
+router.get('/', getWorkouts);
 // GET a single workout
-router.get('/:id', (req, res) => {
-    res.json({ mssg: ' gET single workout route' });
-})
+router.get('/:id', getWorkout);
 // POST a new workout
-router.post('/', async (req, res) => {
-    const { title, load, reps } = req.body;
-    try {
-        const workout = await Workout.create({ title, load, reps })
-        res.status(200).json(workout);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-
-    res.json({ mssg: 'POST a new workout route' });
-})  
+router.post('/', createWorkout)  
 //dELETE a workout
 router.delete('/:id', (req, res) => {
     res.json({ mssg: 'DELETE a workout route' });
